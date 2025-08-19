a7951dd7bd3a671aa5a10bb3e50e2d1675f0cf99
BenedictJowin <benedict.jowin2005@gmail.com>
2025-08-19 22:15:20 +0530

    Update bin2bcd.v

--- bin2bcd.v
+++ bin2bcd.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.11.2023 12:03:53
-// Design Name: 
-// Module Name: bin2bcd
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module bin2bcd(
    input [13:0] bin,
    output reg [15:0] bcd
@@ -28,7 +8,7 @@ integer i;
 	
 always @(bin) begin
     bcd=0;		 	
-    for (i=0;i<14;i=i+1) begin					//Iterate once for each bit in input number
+    for (i=0;i<14;i=i+1) begin					
         if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		//If any BCD digit is >= 5, add three
 	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
 	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
